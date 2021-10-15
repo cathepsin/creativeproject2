542525cee842080fbc53ae30ebb5713a4f1f97b2
cathepsin <nate.mcmurray13@gmail.com>
2021-10-14 18:20:09 -0600

    commit

--- java.js
+++ java.js
@@ -5,7 +5,7 @@ document.getElementById("stockSubmit").addEventListener("click", function(event)
     return;
   console.log(value);
 
-  const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=APIKEY";
+  const url = "https://api.twelvedata.com/time_series?symbol=" + value + ",EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=a0f9d5c9c5284e5bae1eb967b10927eb";
   fetch(url)
     .then(function(response) {
       return response.json();
